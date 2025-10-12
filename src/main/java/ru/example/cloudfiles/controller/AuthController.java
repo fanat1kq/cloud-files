53cb1183d579ae3687047e17249d5fef9f7817b0
fanat1kq <110695423+fanat1kq@users.noreply.github.com>
2025-10-12 16:17:18 +0300

    refactoring ci cd

--- src/main/java/ru/example/cloudfiles/controller/AuthController.java
+++ src/main/java/ru/example/cloudfiles/controller/AuthController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,10 @@ public class AuthController {
                                   HttpServletResponse httpServletResponse) {
 
         return userService.signIn(request, httpServletRequest, httpServletResponse);
+    }
+
+    @GetMapping("qwe")
+    public String qwe(){
+        return "hello";
     }
 }
